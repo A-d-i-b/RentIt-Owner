f4f532c5d4c4e48e354f7b1f7308feb023e7578e
Priyanshu Sharma <inbox.priyanshu@gmail.com>
2022-10-05 18:55:46 +0530

    fixed spellings, added generate function

--- lib/Forms/pg_form.dart
+++ lib/Forms/pg_form.dart
@@ -30,99 +30,20 @@ class _PgFormState extends State<PgForm> {
     "Power Backup": "N/A", //index 0
     "AC Rooms": "N/A", //index 1
     "Maintenance": "N/A", //index 2
-    "Electicity Charges": "N/A", //index 3
+    "Electricity Charges": "N/A", //index 3
     "Available for": "N/A", // index 4
-    "Preferd Tenants": "N/A", // index 5
+    "Preferred Tenants": "N/A", // index 5
     "Food": "N/A", // index 6
     "Wifi": "N/A", // index 7
     "Furniture": "N/A", // else
   };
 
-  List<DropdownMenuItem<String>> iteams_0 = [
-    const DropdownMenuItem(
-      value: "N/A",
-      child: Text("N/A"),
-    ),
-    const DropdownMenuItem(
-      value: "Students",
-      child: Text("Students"),
-    ),
-  ];
-  List<DropdownMenuItem<String>> iteams_1 = [
-    const DropdownMenuItem(
-      value: "N/A",
-      child: Center(
-        child: Text(
-          "N/A",
-        ),
-      ),
-    ),
-    const DropdownMenuItem(
-      value: "Available",
-      child: Text("Available"),
-    ),
-    const DropdownMenuItem(
-      value: "Not Available",
-      child: Text("Not Available"),
-    )
-  ];
-  List<DropdownMenuItem<String>> iteams_2 = [
-    const DropdownMenuItem(
-      value: "N/A",
-      child: Text("N/A"),
-    ),
-    const DropdownMenuItem(
-      value: "Included",
-      child: Text("Included"),
-    ),
-    const DropdownMenuItem(
-      value: "Not Included",
-      child: Text("Not Included"),
-    ),
-  ];
-  List<DropdownMenuItem<String>> iteams_3 = [
-    const DropdownMenuItem(
-      value: "N/A",
-      child: Text("N/A"),
-    ),
-    const DropdownMenuItem(
-      value: "Male",
-      child: Text("Male"),
-    ),
-    const DropdownMenuItem(
-      value: "Female",
-      child: Text("Female"),
-    ),
-    const DropdownMenuItem(
-      value: "Both",
-      child: Text("Both"),
-    ),
-  ];
-  List<DropdownMenuItem<String>> iteams_4 = [
-    const DropdownMenuItem(
-      value: "N/A",
-      child: Text("N/A"),
-    ),
-    const DropdownMenuItem(
-      value: "Furnished",
-      child: Text("Furnished"),
-    ),
-    const DropdownMenuItem(
-      value: "Semi Furnished",
-      child: Text("Semi Furnished"),
-    ),
-    const DropdownMenuItem(
-      value: "Not Furnished",
-      child: Text("Not Furnished"),
-    ),
-  ];
-
-  // final iteams_0 = generateItems(["N/A", "Students"]);
-  // final iteams_1 = generateItems(["N/A", "Available", "Not Available"]);
-  // final iteams_2 = generateItems(["N/A", "Included", "Not Included"]);
-  // final iteams_3 = generateItems(["N/A", "Male", "Female", "Both"]);
-  // final iteams_4 =
-  //     generateItems(["N/A", "Furnished", "Semi Furnished", "Not Furnished"]);
+  final iteams_0 = generateItems(["N/A", "Students"]);
+  final iteams_1 = generateItems(["N/A", "Available", "Not Available"]);
+  final iteams_2 = generateItems(["N/A", "Included", "Not Included"]);
+  final iteams_3 = generateItems(["N/A", "Male", "Female", "Both"]);
+  final iteams_4 =
+      generateItems(["N/A", "Furnished", "Semi Furnished", "Not Furnished"]);
 
   Widget dropdownButton(String name, List<DropdownMenuItem<String>> iteam,
           Map<String, String> Details) =>
@@ -133,7 +54,7 @@ class _PgFormState extends State<PgForm> {
           const SizedBox(height: 6),
           Container(
             width: MediaQuery.of(context).size.width / 2.5,
-            height: 40,
+            height: 50,
             padding: const EdgeInsets.all(6),
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10),
@@ -531,3 +452,83 @@ List<DropdownMenuItem<String>> generateItems(List<String> items) {
 
   return generatedItems;
 }
+
+
+// List<DropdownMenuItem<String>> iteams_0 = [
+  //   const DropdownMenuItem(
+  //     value: "N/A",
+  //     child: Text("N/A"),
+  //   ),
+  //   const DropdownMenuItem(
+  //     value: "Students",
+  //     child: Text("Students"),
+  //   ),
+  // ];
+  // List<DropdownMenuItem<String>> iteams_1 = [
+  //   const DropdownMenuItem(
+  //     value: "N/A",
+  //     child: Center(
+  //       child: Text(
+  //         "N/A",
+  //       ),
+  //     ),
+  //   ),
+  //   const DropdownMenuItem(
+  //     value: "Available",
+  //     child: Text("Available"),
+  //   ),
+  //   const DropdownMenuItem(
+  //     value: "Not Available",
+  //     child: Text("Not Available"),
+  //   )
+  // ];
+  // List<DropdownMenuItem<String>> iteams_2 = [
+  //   const DropdownMenuItem(
+  //     value: "N/A",
+  //     child: Text("N/A"),
+  //   ),
+  //   const DropdownMenuItem(
+  //     value: "Included",
+  //     child: Text("Included"),
+  //   ),
+  //   const DropdownMenuItem(
+  //     value: "Not Included",
+  //     child: Text("Not Included"),
+  //   ),
+  // ];
+  // List<DropdownMenuItem<String>> iteams_3 = [
+  //   const DropdownMenuItem(
+  //     value: "N/A",
+  //     child: Text("N/A"),
+  //   ),
+  //   const DropdownMenuItem(
+  //     value: "Male",
+  //     child: Text("Male"),
+  //   ),
+  //   const DropdownMenuItem(
+  //     value: "Female",
+  //     child: Text("Female"),
+  //   ),
+  //   const DropdownMenuItem(
+  //     value: "Both",
+  //     child: Text("Both"),
+  //   ),
+  // ];
+  // List<DropdownMenuItem<String>> iteams_4 = [
+  //   const DropdownMenuItem(
+  //     value: "N/A",
+  //     child: Text("N/A"),
+  //   ),
+  //   const DropdownMenuItem(
+  //     value: "Furnished",
+  //     child: Text("Furnished"),
+  //   ),
+  //   const DropdownMenuItem(
+  //     value: "Semi Furnished",
+  //     child: Text("Semi Furnished"),
+  //   ),
+  //   const DropdownMenuItem(
+  //     value: "Not Furnished",
+  //     child: Text("Not Furnished"),
+  //   ),
+  // ];
