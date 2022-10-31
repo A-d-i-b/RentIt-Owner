9ee0feb8639e9584ef10de540a4e620101b6d782
A-d-i-b <adibsiddiqui720@gmail.com>
2022-10-31 17:06:17 +0530

    Added Coming Soon screen

--- lib/Screens/tenant_list_screen.dart
+++ lib/Screens/tenant_list_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:househunt/utils/tenant_card.dart';
@@ -17,14 +19,14 @@ class _TenantListState extends State<TenantList> {
       name: "Tenant Name",
       pgName: "Pg name",
       onTap: () {
-        Get.toNamed('/tenant-profile', arguments: {
-          "TenantImage": const AssetImage(
-              "images/tenantexample.jpeg"), //TODO:Pass Actual Parameters
-          "TenantName": "Tenant Name",
-          "PhoneNumber": "8862732723",
-          "PgName": "Pg/flat Name",
-          "Address": "Tenant Address",
-        });
+        // Get.toNamed('/tenant-profile', arguments: {
+        //   "TenantImage": const AssetImage(
+        //       "images/tenantexample.jpeg"), //TODO:Pass Actual Parameters
+        //   "TenantName": "Tenant Name",
+        //   "PhoneNumber": "8862732723",
+        //   "PgName": "Pg/flat Name",
+        //   "Address": "Tenant Address",
+        // });
       },
     ),
     TenantCard(
@@ -32,13 +34,13 @@ class _TenantListState extends State<TenantList> {
       name: "Tenant Name",
       pgName: "Pg name",
       onTap: () {
-        Get.toNamed('/tenant-profile', arguments: {
-          "TenantImage": const AssetImage("images/tenantexample.jpeg"),
-          "TenantName": "Tenant Name",
-          "PhoneNumber": "8862732723",
-          "PgName": "Pg/flat Name",
-          "Address": "Tenant Address",
-        });
+        // Get.toNamed('/tenant-profile', arguments: {
+        //   "TenantImage": const AssetImage("images/tenantexample.jpeg"),
+        //   "TenantName": "Tenant Name",
+        //   "PhoneNumber": "8862732723",
+        //   "PgName": "Pg/flat Name",
+        //   "Address": "Tenant Address",
+        // });
       },
     )
   ];
@@ -55,21 +57,37 @@ class _TenantListState extends State<TenantList> {
           style: Get.textTheme.headline4,
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 20.0),
-        child: ListView.separated(
-          itemCount: cards.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: cards[index],
-            );
-          },
-          separatorBuilder: (BuildContext context, int index) {
-            return const SizedBox(height: 10);
-          },
+      body: Stack(children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 20.0),
+          child: ListView.separated(
+            itemCount: cards.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: cards[index],
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return const SizedBox(height: 10);
+            },
+          ),
         ),
-      ),
+        BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+          child: SizedBox(
+            width: Get.width,
+            height: Get.height,
+            child: const Center(
+                child: Text(
+              "Coming Soon",
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            )),
+          ),
+        )
+      ]),
     );
   }
 }
